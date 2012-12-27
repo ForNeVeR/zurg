b7c311c234ded6a93908c0d969648605a3f83f23
ForNeVeR <neverthness@gmail.com>
2012-12-27 23:26:05 +0700

    Initial version.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject zurg "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.logic "0.7.5"]]
+  :main zurg/main)
---
+++ src/zurg/main.clj
@@ -0,0 +1,4 @@
+(ns zurg.main)
+
+(defn -main [& args]
+)
