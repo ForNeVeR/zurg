0180f534dbac9d2e2706b53a8a54d2be5a1d1538
ForNeVeR <neverthness@gmail.com>
2012-12-31 15:46:16 +0700

    main: step function.

--- src/zurg/main.clj
+++ src/zurg/main.clj
@@ -8,13 +8,31 @@
 
 (def boundary 60)
 
+(defn step [left-side right-side route]
+  (conde
+    ((== left-side []))
+    ((membero :light left-side)
+     (fresh [left-side2 right-side2 route2 traveler1 traveler2]
+       (membero traveler1 left-side)
+       (membero traveler2 left-side)
+       (=/= traveler1 traveler2)
+       ;; TODO: remove t1 and t2 from the left side
+       (appendo right-side [traveler1 traveler2] right-side2)
+       (appendo route [traveler2 traveler2] route2)
+       (step left-side2 right-side2 route2)))
+    ((membero :light right-side)
+     (fresh [left-side2 right-side2 route2 traveler]
+       (membero traveler right-side)
+       (appendo left-side [:light traveler] left-side2)
+       ;; TODO: Define right-side2 as the right-side without the traveler
+       (appendo route [toy] route2)
+       (step left-side2 right-side2 router2)))))
+
 (defn do-solve []
-  (run* [route]
+  (run 1 [route]
+    (step [:buzz :woody :rex :hamm :light] [] route)
     (fresh [time]
-      (conde
-        ((== time 30) (== route 1))
-        ((== time 60) (== route 2))
-        ((== time 90) (== route 3)))
+      ;; TODO: calculate travel time from the route variable
       (project [time]
         (== true (<= time boundary))))))
 
