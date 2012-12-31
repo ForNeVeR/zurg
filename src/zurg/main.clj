4afa631b039b357307323359389b5743a5cc1ee2
ForNeVeR <neverthness@gmail.com>
2012-12-31 17:36:50 +0700

    main: rembero and removeo functions implemented, step function (almost?) finished.

--- src/zurg/main.clj
+++ src/zurg/main.clj
@@ -8,30 +8,51 @@
 
 (def boundary 60)
 
-(defn step [left-side right-side route]
+(defn rembero [x ls out]
   (conde
-    ((== left-side []))
+    ((== [] ls) (== [] out))
+    ((fresh [a d]
+       (conso a d ls)
+       (== a x)
+       (== d out)))
+    ((fresh [a d res]
+       (conso a d ls)
+       (!= a x)
+       (conso a res out)
+       (rembero x d res)))))
+
+(defn removeo [elements ys out]
+  (conde
+    ((== elements [])
+     (== out ys))
+    ((fresh [head tail res]
+       (conso head tail elements)
+       (rembero head ys res)
+       (removeo tail res out)))))
+
+(defn step [left-side right-side route out]
+  (conde
+    ((== left-side [])
+     (== out route))
     ((membero :light left-side)
      (fresh [left-side2 right-side2 route2 traveler1 traveler2]
        (membero traveler1 left-side)
        (membero traveler2 left-side)
-       (=/= traveler1 traveler2)
-
-       ;; Remove two elements from left-side and associate result with left-side2:
-       (fresh [tmp]
-         (rembero traveler1 left-side tmp)
-         (rembero traveler2 tmp left-side2))
-
-       (appendo right-side [traveler1 traveler2] right-side2)
-       (appendo route [traveler2 traveler2] route2)
-       (step left-side2 right-side2 route2)))
+       (!= traveler1 :light)
+       (!= traveler2 :light)
+       (!= traveler1 traveler2)
+       (removeo [traveler1 traveler2 :light] left-side left-side2)
+       (appendo right-side [traveler1 traveler2 :light] right-side2)
+       (appendo route [[traveler1 traveler2]] route2)
+       (step left-side2 right-side2 route2 out)))
     ((membero :light right-side)
      (fresh [left-side2 right-side2 route2 traveler]
        (membero traveler right-side)
+       (!= traveler :light)
        (appendo left-side [:light traveler] left-side2)
-       (rembero traveler right-side right-side2)
-       (appendo route [toy] route2)
-       (step left-side2 right-side2 router2)))))
+       (removeo [traveler :light] right-side right-side2)
+       (appendo route [traveler] route2)
+       (step left-side2 right-side2 route2 out)))))
 
 (defn do-solve []
   (run 1 [route]
